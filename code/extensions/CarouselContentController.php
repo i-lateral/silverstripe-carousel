3425b62820b0a3813b712c7021adf6d5aa05b99a
Morven Lewis-Everley <morven@i-lateral.com>
2012-12-19 11:58:03 +0000

    Update requirements URL's to SS3

--- code/extensions/CarouselContentController.php
+++ code/extensions/CarouselContentController.php
@@ -1,18 +1,13 @@
 <?php
 
 class CarouselContentController extends Extension {
-    public function onBeforeInit() {
-        // Disable HASH rewriting
-        SSViewer::dontRewriteHashlinks();
-    
+    public function onBeforeInit() {    
         // Load required CSS
         Requirements::css('carousel/css/carousel.css');
     
         // Load required javascript 
-        Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery-packed.js');
-        Requirements::javascript(THIRDPARTY_DIR . '/jquery-ui/jquery.ui.core.js');
-        Requirements::javascript(THIRDPARTY_DIR . '/jquery-ui/jquery.ui.widget.js');
-        Requirements::javascript(THIRDPARTY_DIR . '/jquery-ui/jquery.ui.tabs.js');
+        Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
+        Requirements::javascript(THIRDPARTY_DIR . '/jquery-ui/jquery.ui.js');
         Requirements::javascript('carousel/javascript/carousel.js');
     }
 }
