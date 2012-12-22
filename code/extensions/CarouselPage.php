5e8bc244a9d023b1f2fd101c1529ed76783a4038
Morven Lewis-Everley <morven@i-lateral.com>
2012-12-22 16:30:37 +0000

    Add show carousel setting, so that carousel section onlt loads when needed

--- code/extensions/CarouselPage.php
+++ code/extensions/CarouselPage.php
@@ -1,28 +1,46 @@
 <?php
 
 class CarouselPage extends DataExtension {
+    public static $db = array(
+        'ShowCarousel'  => 'Boolean'
+    );
+    
     public static $has_many = array(
         'Slides' => 'CarouselSlide'
     );
     
     public function updateCMSFields(FieldList $fields) {
 		
-		// Add calc options
-        $grid_config = GridFieldConfig::create();
-        $grid_config->addComponents(
-            new GridFieldAddnewButton(),
-            new GridFieldDeleteAction(),
-            new GridFieldDataColumns(),
-            new GridFieldDetailForm(),
-            new GridFieldEditButton(),
-            new GridFieldSortableHeader()
-        );
-        
-        $carousel_table = GridField::create('Slides', null, $this->owner->Slides()->sort('Sort DESC'), $grid_config);
+		if($this->owner->ShowCarousel) {
+		    // Add calc options
+            $grid_config = GridFieldConfig::create();
+            $grid_config->addComponents(
+                new GridFieldAddnewButton(),
+                new GridFieldDeleteAction(),
+                new GridFieldDataColumns(),
+                new GridFieldDetailForm(),
+                new GridFieldEditButton(),
+                new GridFieldSortableHeader()
+            );
+            
+            $carousel_table = GridField::create('Slides', null, $this->owner->Slides()->sort('Sort DESC'), $grid_config);
 		
-		$fields->addFieldToTab('Root.Carousel', $carousel_table);
+		    $fields->addFieldToTab('Root.Carousel', $carousel_table);
+		} else {
+		    $fields->removeByName('Slides');
+		}
+		
+		$fields->removeByName('ShowCarousel');
         
         parent::updateCMSFields($fields);
+    }
+    
+    public function updateSettingsFields(FieldList $fields) {
+	    $carousel = FieldGroup::create(
+	        CheckboxField::create('ShowCarousel', 'Show a carousel on this page?')
+		)->setTitle('Carousel');
+		
+	    $fields->addFieldToTab('Root.Settings', $carousel);
     }
     
     public function CarouselSlides() {
