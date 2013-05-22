20484b2483e9f22f8df46af588a5a5ed5bb77b26
Morven Lewis-Everley <morven@i-lateral.com>
2013-05-22 11:14:40 +0100

    Remove jquery to handle opacity making it easier for users to set their own

--- javascript/carousel.js
+++ javascript/carousel.js
@@ -1,48 +1,48 @@
 (function($) {
-	$(document).ready(function() {
-		$('.carousel-slides')
-			.tabs({
-				activate: function(event, ui) {					
-					new_left = $(this).width() + 'px';
-					old_left = '-' + $(this).width() + 'px';
-                    
+    $(document).ready(function() {
+        $('.carousel-slides')
+            .tabs({
+                activate: function(event, ui) {
+                    new_left = $(this).width() + 'px';
+                    old_left = '-' + $(this).width() + 'px';
+
                     // If we are at the end of the tabs, loop back around
-					if(ui.oldPanel) {
-						new_index = ui.newPanel.index();
-						old_index = ui.oldPanel.index();
-						
-						if(new_index < old_index) {
-							new_left = '-' + $(this).width() + 'px';
-							old_left = $(this).width() + 'px';
-						}
-					}    
-					
-					// Dont use the builtin fx effects.          
-					$(ui.newPanel)
-						.css('left',new_left)
-						.show()
-						.animate({ left: 0 }, 500);
-					
-					if(ui.oldPanel) {
-						ui.oldPanel
+                    if(ui.oldPanel) {
+                        new_index = ui.newPanel.index();
+                        old_index = ui.oldPanel.index();
+
+                        if(new_index < old_index) {
+                            new_left = '-' + $(this).width() + 'px';
+                            old_left = $(this).width() + 'px';
+                        }
+                    }
+
+                    // Dont use the builtin fx effects.
+                    $(ui.newPanel)
+                        .css('left',new_left)
+                        .show()
+                        .animate({ left: 0 }, 500);
+
+                    if(ui.oldPanel) {
+                        ui.oldPanel
                             .show()
                             .animate({ left: old_left }, 500);
-					}
-				}
-			})
-			.tabs('rotate', 5000, true);
-			
-			
-		// Add hover effect to stop tab animation
-		$('.carousel-slides').hover(
-			function() {
-				$(this).tabs('rotate', 0, false);
-			}, function() {
-				$(this).tabs('rotate', 5000, true);
-			}
-		);
-		
-		// Set transparency on h2's
-		$('.carousel-slides .carousel-slide .carousel-slide-content').css({ opacity: 0.75 });
-	});
+                    }
+                }
+            })
+            .tabs('rotate', 5000, true);
+
+
+        // Add hover effect to stop tab animation
+        $('.carousel-slides').hover(
+            function() {
+                $(this).tabs('rotate', 0, false);
+            }, function() {
+                $(this).tabs('rotate', 5000, true);
+            }
+        );
+
+        // Add the line below to your own JS to set the opacity of the carousel
+        //$('.carousel-slides .carousel-slide .carousel-slide-content').css({ opacity: 0.75 });
+    });
 })(jQuery)
