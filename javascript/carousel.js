0511ccf97f827b335c3e29582a7745f147a6d9dd
Morven Lewis-Everley <morven@i-lateral.com>
2014-02-06 16:12:56 +0000

    Upgrade to using flexslider for default carousel

--- javascript/carousel.js
+++ javascript/carousel.js
@@ -1,48 +1,14 @@
 (function($) {
     $(document).ready(function() {
-        $('.carousel-slides')
-            .tabs({
-                activate: function(event, ui) {
-                    new_left = $(this).width() + 'px';
-                    old_left = '-' + $(this).width() + 'px';
-
-                    // If we are at the end of the tabs, loop back around
-                    if(ui.oldPanel) {
-                        new_index = ui.newPanel.index();
-                        old_index = ui.oldPanel.index();
-
-                        if(new_index < old_index) {
-                            new_left = '-' + $(this).width() + 'px';
-                            old_left = $(this).width() + 'px';
-                        }
-                    }
-
-                    // Dont use the builtin fx effects.
-                    $(ui.newPanel)
-                        .css('left',new_left)
-                        .show()
-                        .animate({ left: 0 }, 500);
-
-                    if(ui.oldPanel) {
-                        ui.oldPanel
-                            .show()
-                            .animate({ left: old_left }, 500);
-                    }
-                }
-            })
-            .tabs('rotate', 5000, true);
-
-
-        // Add hover effect to stop tab animation
-        $('.carousel-slides').hover(
-            function() {
-                $(this).tabs('rotate', 0, false);
-            }, function() {
-                $(this).tabs('rotate', 5000, true);
-            }
-        );
+        /**
+         * Flexslider animation
+         */
+        $('.flexslider').flexslider({
+            animation: "slide",
+            selector: ".slides > .slide"
+        });
 
         // Add the line below to your own JS to set the opacity of the carousel
-        //$('.carousel-slides .carousel-slide .carousel-slide-content').css({ opacity: 0.75 });
+        $('.flexslider .slide .slide-content').css({ opacity: 0.75 });
     });
 })(jQuery)
--- templates/Includes/CarouselSlides.ss
+++ templates/Includes/CarouselSlides.ss
@@ -1,25 +1,25 @@
+<% require css(carousel/css/flexslider.css) %>
 <% require css(carousel/css/carousel.css) %>
 
 <% require javascript(framework/thirdparty/jquery/jquery.js) %>
-<% require javascript(framework/thirdparty/jquery-ui/jquery-ui.js) %>
-<% require javascript(carousel/javascript/jquery-ui-tabs-rotate.js) %>
+<% require javascript(carousel/javascript/jquery.flexslider-min.js) %>
 <% require javascript(carousel/javascript/carousel.js) %>
 
-<% if Slides.Exists %><div class="carousel-container">
-    <div class="carousel-slides">
-        <% loop $Slides %>
-            <div id="carousel-slide-$Pos" class="carousel-slide $FirstLast">
-                $SizedImage
-                <% if Title %><div class="carousel-slide-content"><h2>$Title.RAW</h2></div><% end_if %>
-            </div>
-        <% end_loop %>
+<% if Slides.Exists %>
+    <div class="flexslider">
+        <div class="slides">
+            <% loop $Slides %>
+                <div id="carousel-slide-$Pos" class="slide $FirstLast">
 
-        <div class="carousel-controls">
-            <ul class="carousel-controls-list">
-                <% loop $Slides %>
-                    <li class="carousel-control $FirstLast"><a href="#carousel-slide-{$Pos}"><span>$Pos</span></a></li>
-                <% end_loop %>
-            </ul>
+                    $SizedImage
+
+                    <% if Title %>
+                        <div class="slide-content">
+                            <h2>$Title.RAW</h2>
+                        </div>
+                    <% end_if %>
+                </div>
+            <% end_loop %>
         </div>
     </div>
-</div><% end_if %>
+<% end_if %>
