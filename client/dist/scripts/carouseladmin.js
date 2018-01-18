f6b8dcfa63b3a184464a00de605f65b85ebf2b51
Morven Lewis-Everley <morven@i-lateral.com>
2018-01-18 16:12:03 +0000

    Add JS to show and hide needed fields in the CMS

--- client/dist/scripts/carouseladmin.js
+++ client/dist/scripts/carouseladmin.js
@@ -1,9 +1,34 @@
 (function($) {
     $.entwine("ss", function($) {
+        // cache previous search results
+        var searchCache = {};
+
+        var display_fields = [
+            "#Form_EditForm_CarouselShowIndicators_Holder",
+            "#Form_EditForm_CarouselShowControls_Holder",
+            "#Form_EditForm_CarouselWidth_Holder",
+            "#Form_EditForm_CarouselHeight_Holder",
+            "#Form_EditForm_CarouselInterval_Holder",
+        ];
+
         $("input#Form_EditForm_ShowCarousel").entwine({
-            onmatch: function (event) {
-                alert("item");
-            }
+            onmatch: function() {
+                this.togglefields();
+            },
+            onchange: function() {
+                this.togglefields();
+            },
+            togglefields: function()
+            {
+                for (var i in display_fields) {
+                    var field = $(display_fields[i]);
+                    if ($(this).is(":checked")) {
+                        field.show();
+                    } else {
+                        field.hide();
+                    }
+                }
+            },
         });
     });
 })(jQuery);
